ee7609c191085acbddb7fe49f68386c0b8f769c4
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-12-09 19:05:47 -0300

    Fixed a few mistakes

--- src/modules/boleto/services/ValidarBoletoService.spec.ts
+++ src/modules/boleto/services/ValidarBoletoService.spec.ts
@@ -44,7 +44,7 @@ describe('ValidarBoletoService', () => {
     ).rejects.toBeInstanceOf(AppError);
   });
 
-  it(`shouldn't accept codes with letters`, async () => {
+  it(`shouldn't accept codes with letters or special characters`, async () => {
     const typedCode = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
 
     await expect(
@@ -57,3 +57,17 @@ describe('ValidarBoletoService', () => {
 });
 
 
+//  836000000015460201103138834403604020100240230860 - Convênio
+//  848800000027548301622023012101193681585024111229 - Convênio
+//  111111111111222222222222333333333333444444444444
+
+//  21290001192110001210904475617405975870000002000 - (47) - Enviado na URL
+//  21299758700000020000001121100012100447561740 - (44) - Teste
+//  11111111112222222222233333333333455555555555555
+
+
+// 21290001192110001210904475617405975870000002000
+
+//   “barCode”: “21299758700000020000001121100012100447561740”,
+//   “amount”: “20.00”,
+//   “expirationDate”: “2018 - 07 - 16”
