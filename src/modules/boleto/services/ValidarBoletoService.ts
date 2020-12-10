1a54a68c0aa311ab4d0440e55a6359794e40c6f4
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-12-09 23:32:40 -0300

    Finished generating barCode to Título

--- src/modules/boleto/services/ValidarBoletoService.ts
+++ src/modules/boleto/services/ValidarBoletoService.ts
@@ -13,13 +13,12 @@ interface IResponse {
 class ValidarBoletoService {
   constructor() {}
 
-  public async run({ typedCode }: IRequest): Promise<IResponse> {
-    const acceptedCodeLenghts = [47, 48];   //  =>  Título / Convênio (Presumindo-se que os valores estarão sempre em ordem crescente)
+  public async run({ typedCode }: IRequest): Promise<IResponse | undefined> {
+    const acceptedCodeLenghts = [47, 48]; //  =>  Título / Convênio
 
-    const isValid = (
+    const isValid =
       acceptedCodeLenghts.includes(typedCode.length) && //  Verifica se o tamanho do código está entre os tamanhos aceitáveis
-      typedCode.match(/^[0-9]+$/) != null //  Verifica se só existem números na string
-    );
+      typedCode.match(/^[0-9]+$/) != null; //  Verifica se só existem números na string
 
     if (!isValid) {
       let errorMessage = '';
@@ -33,7 +32,7 @@ class ValidarBoletoService {
         errorMessage = `Code is too short`;
       }
 
-      if (typedCode.length > acceptedCodeLenghts[acceptedCodeLenghts.length - 1]) {
+      if (typedCode.length > acceptedCodeLenghts[1]) {
         // Confere o maior valor possível
         errorMessage = 'Code is too long';
       }
@@ -41,13 +40,58 @@ class ValidarBoletoService {
       throw new AppError(errorMessage);
     }
 
-    const boleto = {
-      codeBar: typedCode,
-      amount: '20.00',
-      expirationDate: '2018-07-16',
-    };
+    const tipoBoleto = typedCode.length === 47 ? 'TITULO' : 'CONVENIO';
 
-    return boleto;
+    if (tipoBoleto === 'TITULO') {
+      //  21290001192110001210904475617405975870000002000 =>  Código usado
+
+      //  =>  Código quebrado em campos
+      const campo1 = typedCode.slice(0, 10); //  2129000119
+      const campo2 = typedCode.slice(10, 21); //  21100012109
+      const campo3 = typedCode.slice(21, 32); //  04475617405
+      const campo4 = typedCode.slice(32, 33); //  9 =>  Digito verificador do código de barras
+      const campo5 = typedCode.slice(33, 47); //  7587 0000002000
+
+      const idBanco = campo1.slice(0, 3);
+      const idMoeda = campo1.slice(3, 4);
+      const barraPositions20to24 = campo1.slice(4, 9);
+      const digitoVerificadorCampo1 = campo1.slice(9, 10);
+
+      const barraPositions25to34 = campo2.slice(0, 10);
+      const digitoVerificadorCampo2 = campo2.slice(10, 11);
+
+      const barraPositions35to44 = campo3.slice(0, 10);
+      const digitoVerificadorCampo3 = campo3.slice(10, 11);
+
+      const fatorVencimento = campo5.slice(0, 4);
+      const barraPositions10to19 = campo5.slice(4, 14);
+
+      //  2129000119 21100012109 04475617405 9 75870000002000
+
+      const codeBar =
+        idBanco +
+        idMoeda +
+        campo4 +
+        fatorVencimento +
+        barraPositions10to19 +
+        barraPositions20to24 +
+        barraPositions25to34 +
+        barraPositions35to44;
+
+      return {
+        codeBar,
+        amount: '20.00',
+        expirationDate: '2018-07-16',
+      };
+    } else if (tipoBoleto === 'CONVENIO') {
+      const codeBar = typedCode;
+
+      return {
+        codeBar,
+        amount: '20.00',
+        expirationDate: '2018-07-16',
+      };
+    }
   }
 }
 
